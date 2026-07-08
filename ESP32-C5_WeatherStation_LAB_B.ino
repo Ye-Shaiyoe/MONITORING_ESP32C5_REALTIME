0eb6294f780b9f62ad2635aec7853af228a9a93d
Akram <akrommuhammadyusuf@gmail.com>
2026-07-08 22:03:02 +0700

    Update and rename ESP32-C5_WeatherStation_LAB_B to ESP32-C5_WeatherStation_LAB_B.ino

--- ESP32-C5_WeatherStation_LAB_B.ino
+++ ESP32-C5_WeatherStation_LAB_B.ino
@@ -14,8 +14,8 @@
 // Jaringan 
 const char* ssid     = "MAIN MASS";
 const char* password = "massaK46";
-const char* host     = "percobaanta1hares.my.id";
-const String path    = "/F01d3rD4t4k0D3L4BBPSUML/kirimdata/kirimdata3.php";
+const char* host     = "web.my.id";
+const String path    = "/Lpath/path/kirimdata2.php";
 
 // Hardware 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
