7c9276f45fe79a98a23f83400dd3b57331586f7e
Akram <akrommuhammadyusuf@gmail.com>
2026-07-08 22:03:42 +0700

    Update ESP32-C5_WeatherStation_LAB_K46-FIX.ino

--- ESP32-C5_WeatherStation_LAB_K46-FIX.ino
+++ ESP32-C5_WeatherStation_LAB_K46-FIX.ino
@@ -12,8 +12,8 @@
 
 const char* ssid     = "MAIN MASS";
 const char* password = "massaK46";
-const char* host     = "percobaanta1hares.my.id";
-const String path    = "/F01d3rD4t4k0D3L4BBPSUML/kirimdata/kirimdata2.php";
+const char* host     = "web.my.id";
+const String path    = "/Lpath/path/kirimdata2.php";
 
 // Hardware 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
